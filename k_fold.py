ad360911be1f22bee47094870b8600883793ee2c
francois-zan <fzanella80@gmail.com>
2025-12-09 14:23:23 +0100

    Update k_fold.py

--- k_fold.py
+++ k_fold.py
@@ -1,14 +1,22 @@
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier  # knn
+from sklearn.tree import DecisionTreeClassifier     # tree
 
 
+def make_model(model, parameter):
+    if model == 'knn':
+        return KNeighborsClassifier(n_neighbors=parameter)
+    if model == 'tree':
+        return DecisionTreeClassifier(max_depth=parameter)
+    else:
+        return
+        
 
-
-def k_fold_cv_tree(k, depth, x_learn, y_learn):
+def k_fold_cv(k, parameter, model, x_learn, y_learn):
     """
     Returns the cross validation error for a given number 
-    of subsets k, a given depth and a given learning datset.
+    of subsets k, a given parameter for the considered model
+    and a given learning dataset.
 
     """
     errors = np.zeros(k)
@@ -19,37 +27,18 @@ def k_fold_cv_tree(k, depth, x_learn, y_learn):
         del x_to_fit[i]
         y_to_fit = y_subsets.copy()
         del y_to_fit[i]
-
-        tree = DecisionTreeClassifier(max_depth=depth)
-        tree.fit(np.concatenate(x_to_fit, axis=0),
-                 np.concatenate(y_to_fit, axis=0))
-        errors[i] = 1 - tree.score(x, y)
-
-    return np.mean(errors)
-
-
-def k_fold_cv_knn(k, n_neighbors, x_learn, y_learn):
-    """
-     Returns the cross validation error for a given number 
-     of subsets k, a given number of neighbors and a given learning datset.
-
-    """
-    errors = np.zeros(k)
-    x_subsets = np.array_split(x_learn, k)
-    y_subsets = np.array_split(y_learn, k)
-    for x, y, i in zip(x_subsets, y_subsets, range(k)):
-        x_to_fit = x_subsets.copy()
-        del x_to_fit[i]
-        y_to_fit = y_subsets.copy()
-        del y_to_fit[i]
-
+        
         x_to_fit = np.concatenate(x_to_fit, axis=0)
         y_to_fit = np.concatenate(y_to_fit, axis=0)
+        
+        if model == 'knn':
+            if parameter >= len(y_to_fit):
+                parameter = len(y_to_fit) - 1
+        
+        model = make_model(model, parameter)
+        model.fit(np.concatenate(x_to_fit, axis=0),
+                 np.concatenate(y_to_fit, axis=0))
+        errors[i] = 1 - model.score(x, y)
+    return np.mean(errors), np.var(errors)
 
-        if n_neighbors >= len(y_to_fit):
-            n_neighbors = len(y_to_fit) - 1
-        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-        knn.fit(x_to_fit, y_to_fit)
-        errors[i] = 1 - knn.score(x, y)
 
-    return np.mean(errors)
