9791e4364a203c96adde8cd7cdc184d83d5431b0
francois-zan <fzanella80@gmail.com>
2025-12-16 17:34:40 +0100

    Add KNN and SVM classifiers to models.py

--- models.py
+++ models.py
@@ -2,15 +2,21 @@ import numpy as np
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.svm import SVC 
 
 
 def tree(X_LS_pairs, y_LS_pairs, max_depth):
     clf = DecisionTreeClassifier(max_depth=max_depth)
-    clf.fit(X_LS_pairs, y_LS_pairs)
+    clf.fit(X_LS_pairs, y_LS_pairs.values)
     return clf
 
 def knn(X_LS_pairs, y_LS_pairs, n_neighbors):
-    return
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_LS_pairs, y_LS_pairs.values)
+    return clf
 
-def svm(X_LS_pairs, y_LS_pairs, parameter):
-    return
+def svm(X_LS_pairs, y_LS_pairs, C):
+    clf = SVC(C = C, probability=True)
+    clf.fit(X_LS_pairs, y_LS_pairs.values)
+    return clf
